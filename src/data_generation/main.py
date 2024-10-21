68898fe7dbf95e6c9649189184e7337a611261a7
Rakshith Dharmappa <85234035+Rakshith2605@users.noreply.github.com>
2024-10-21 19:09:23 -0400

    added scrape_save.py to main

--- src/data_generation/main.py
+++ src/data_generation/main.py
@@ -1,6 +1,8 @@
 from Get_all_url import get_all_links
-
+from Scrape import scrape_and_save
 documentation_url = "https://rc-docs.northeastern.edu/en/latest/index.html"
 
 links = get_all_links(documentation_url)
 print(f"Fetched {len(links)} links")
+#print(links)
+scrape_and_save(links, "scraped_content.txt")
