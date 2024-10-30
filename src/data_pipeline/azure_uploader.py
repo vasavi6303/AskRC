887fe495c6139a3fd2adb59e18336f16c55efb3a
vasavi6303 <vasavisai333@gmail.com>
2024-10-30 15:18:35 -0400

    upload processed file to Azure

--- src/data_pipeline/azure_uploader.py
+++ src/data_pipeline/azure_uploader.py
@@ -4,15 +4,52 @@ azure_uploader.py
 This module uploads preprocessed data to Azure Blob Storage. 
 It provides a function to upload files to a specified container in Azure Blob Storage.
 """
+import os
+from dotenv import load_dotenv
 
+load_dotenv()
+
+from azure.identity import ClientSecretCredential
 from azure.storage.blob import BlobServiceClient
 
-def upload_to_blob(file_path, container_name, connection_string):
+
+client_id = os.getenv('AZURE_CLIENT_ID')
+client_secret_value = os.getenv('AZURE_CLIENT_SECRET_VALUE')
+tenant_id = os.getenv('AZURE_TENANT_ID')
+storage_account_url = os.getenv('AZURE_BLOB_STORAGE_URL')
+
+container_name = "preprocessed-data"
+credentials = ClientSecretCredential(
+    client_id=client_id,
+    client_secret=client_secret_value,
+    tenant_id=tenant_id
+)
+
+
+# def download_from_blob_storage():
+#     blob_service_client = BlobServiceClient(account_url=storage_account_url, credential=credentials)
+#     container_client = blob_service_client.get_container_client(container=container_name)
+#     file_content = container_client.get_blob_client(blob=blob_name).download_blob().readall().decode("utf-8")
+
+def upload_to_blob(file_path, file_name):
     """
     Upload a file to Azure Blob Storage.
     
     Args:
         file_path (str): The path of the file to upload.
-        container_name (str): The name of the Azure Blob container.
-        connection_string (str): The Azure Blob Storage connection string.
     """
+    container_client = getContainerClient(container_name)
+    # Open the file and read its content
+    try:
+        with open(file_path, 'r') as file:
+            container_client.upload_blob(name = file_name, data=file.read())
+            print(file_path + " has been uploaded to blob storage")
+    except FileNotFoundError:
+        print("The file was not found at the specified path.")
+
+def getBlobServiceClient():
+    return BlobServiceClient(account_url=storage_account_url, credential=credentials)
+
+def getContainerClient(container_name):
+    blob_service_client = getBlobServiceClient()
+    return blob_service_client.get_container_client(container=container_name)
